cfbcfcfcd01d271240340af041c05476e556c327
juanserratos <serratos@upenn.edu>
2025-07-25 19:59:13 -0400

    MyTerm Init Commit

---
+++ MyTerm/MyTerm/MyTermApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyTermApp.swift
+//  MyTerm
+//
+//  Created by Juan Angel Serratos on 7/25/25.
+//
+
+import SwiftUI
+
+@main
+struct MyTermApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MyTerm/MyTermTests/MyTermTests.swift
@@ -0,0 +1,17 @@
+//
+//  MyTermTests.swift
+//  MyTermTests
+//
+//  Created by Juan Angel Serratos on 7/25/25.
+//
+
+import Testing
+@testable import MyTerm
+
+struct MyTermTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
